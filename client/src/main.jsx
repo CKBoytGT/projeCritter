c14e39e729a9945443184873ec7d8009dd7d7f18
C K Boyt <ckboyt@gmail.com>
2024-04-01 13:20:36 -0400

    set up new vite client and tailwind

---
+++ client/src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+// apollo client
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+  credentials: "include", // send cookies along with every request to server
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,20 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily: {
-      "sans": ["Montserrat", "ui-sans-serif", "system-ui", "-apple-system", "BlinkMacSystemFont", '"Segoe UI"', "Roboto", '"Helvetica Neue"', "Arial", '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"']
-
-    },
-    extend: {
-      screens: {
-        "3xl": "1920px",
-      },
-      fontFamily: {
-        "display": ["Cherry Bomb One", "cursive"]
-      }
-    },
+    extend: {},
   },
   plugins: [],
 };
-
