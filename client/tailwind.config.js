7d0a4a0e0b351c2884e8490e1e96eed27cb8acd7
C K Boyt <ckboyt@gmail.com>
2024-04-21 14:43:24 -0400

    add custom fonts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: [
+        "Montserrat",
+        "ui-sans-serif",
+        "system-ui",
+        "sans-serif",
+        "Apple Color Emoji",
+        "Segoe UI Emoji",
+        "Segoe UI Symbol",
+        "Noto Color Emoji",
+      ],
+      display: ['"Cherry Bomb One"'],
+    },
     extend: {
       height: {
         screen: ["100vh /* fallback for Opera, IE and etc. */", "100dvh"],
