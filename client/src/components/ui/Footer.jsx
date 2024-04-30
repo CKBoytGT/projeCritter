62a1bfd506572927034f9e341824bc64ae133c47
C K Boyt <ckboyt@gmail.com>
2024-04-30 11:54:31 -0400

    make copyright year dynamic

--- client/src/components/ui/Footer.jsx
+++ client/src/components/ui/Footer.jsx
@@ -1,4 +1,7 @@
 const Footer = () => {
+  const today = new Date();
+  const year = today.getFullYear();
+
   return (
     <footer className="bg-indigo-200 flex justify-center">
       <div className="max-w-7xl w-full px-4 py-2 pb-4 md:p-6 md:pt-4 text-right text-sm font-medium">
@@ -11,7 +14,7 @@ const Footer = () => {
         >
           projeCritter team
         </a>{" "}
-        2023-2024
+        2023-{year}
       </div>
     </footer>
   );
