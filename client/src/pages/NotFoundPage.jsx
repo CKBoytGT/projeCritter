3a07649fae686398e9c480a8fc7e901e9074aac4
C K Boyt <ckboyt@gmail.com>
2024-05-13 18:27:22 -0400

    add not found page

--- client/src/pages/NotFoundPage.jsx
+++ client/src/pages/NotFoundPage.jsx
@@ -1,5 +1,20 @@
 const NotFoundPage = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="flex flex-col justify-center items-center grow w-full h-full">
+      <h2 className="text-9xl text-indigo-500 font-bold drop-shadow-[0.5rem_0.5rem_#bbf7d0]">
+        404
+      </h2>
+      <p className="font-medium">
+        <strong>Error:</strong> We couldn{"'"}t find the page you
+        {"'"}re looking for.
+      </p>
+      <p>
+        <a href="/" className="font-bold hover:underline">
+          « Back to homepage?
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default NotFoundPage;
