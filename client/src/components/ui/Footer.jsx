36d9cebaa21b79286bbefc1061f45e324d054db2
C K Boyt <ckboyt@gmail.com>
2024-07-27 19:04:31 -0400

    update footer link

--- client/src/components/ui/Footer.jsx
+++ client/src/components/ui/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="max-w-7xl w-full px-4 py-2 pb-4 md:p-6 md:pt-4 text-right text-sm font-medium">
         Copyright &#169;{" "}
         <a
-          href="https://github.com/BJThompson12/projeCritter/graphs/contributors"
+          href="https://github.com/CKBoytGT/projeCritter/graphs/contributors"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:underline font-bold"
